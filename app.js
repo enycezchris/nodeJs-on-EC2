241d22594c7e6646a8d31c74f41dbc1b49d57293
enycezchris <enycezchris@gmail.com>
2023-07-08 21:30:30 -0400

    commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const jsonObj = { title: "Hello World!", description: "EC2 Node App!" };
+
 const PORT = 3000 || process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.json(jsonObj);
 });
 
 app.listen(PORT, "0.0.0.0", () => {
