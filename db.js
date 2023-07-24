f9c9aca30097cbe09e9e9902ca00265ae82bc855
enycezchris <enycezchris@gmail.com>
2023-07-24 15:16:53 -0400

    more sequelize psql config

--- db.js
+++ db.js
@@ -2,12 +2,6 @@ const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
 require("dotenv").config();
 
-console.log({
-  dbname: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USERNAME,
-  pw: process.env.DATABASE_PASSWORD,
-});
-
 const db = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USERNAME,
@@ -18,14 +12,16 @@ const db = new Sequelize(
   }
 );
 
-// const setupRDSConnection = async () => {
-//   try {
-//     await db.authenticate();
-//     console.log("Connected to RDS DB");
-//   } catch (error) {
-//     console.error("Not connected to RDS DB");
-//   }
-// };
+const setupRDSConnection = async () => {
+  try {
+    await db.authenticate();
+    console.log("Connected to RDS DB");
+  } catch (error) {
+    console.error("Not connected to RDS DB");
+  }
+};
+
+setupRDSConnection();
 
 const Information = db.define("information", {
   title: {
